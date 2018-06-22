0894a3e028a97d0f1185c3a7b47e16bd9b49327a
Linh Tong <linhthitong@gmail.com>
2018-06-22 15:57:02 -0700

    revise connection

--- server.js
+++ server.js
@@ -9,8 +9,6 @@ var mongoose = require("mongoose");
 var axios = require("axios");
 var cheerio = require("cheerio");
 
-// Require all models
-var db = require("./models");
 
 var PORT = process.env.PORT || 3000;
 
@@ -36,6 +34,9 @@ var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/nyTimes";
 // Connect to the Mongo DB
 mongoose.Promise = Promise;
 mongoose.connect(MONGODB_URI);
+
+// Require all models
+var db = require("./models");
 
 // Routes
 
